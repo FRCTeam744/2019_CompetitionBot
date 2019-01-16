30de3612553f7ec03037705d0df5fac090a0c067
sebmolina <colombiasm24@gmail.com>
2019-01-16 10:53:27 -0500

    cleaned up code

--- SharkAttack/src/main/include/Objects.h
+++ SharkAttack/src/main/include/Objects.h
@@ -1,16 +1,15 @@
-/*
+/*----------------------------------------------------------------------------------*/
 
-All new objects are pointed to here, then the objects are instantiated in RobotInit
+//All new objects are pointed to here, then the objects are instantiated in RobotInit
 
-*/
+/*----------------------------------------------------------------------------------*/
 
 #include <frc/Joystick.h>
 #include <ctre/Phoenix.h>
 #include <frc/RobotDrive.h>
 #include <frc/Preferences.h>
 
-frc::Joystick *rightStick;
-frc::Joystick *leftStick;
+frc::Joystick *rightStick, *leftStick;
 
 WPI_TalonSRX *leftFront, *leftBack, *rightFront, *rightBack;
 
