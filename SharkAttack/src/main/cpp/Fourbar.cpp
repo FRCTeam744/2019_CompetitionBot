3a0ee491dd2a51d4c9dd5fd0aeb1d115958d5483
sebmolina <colombiasm24@gmail.com>
2019-02-17 21:13:33 -0500

    fourbar homing method added

--- SharkAttack/src/main/cpp/Fourbar.cpp
+++ SharkAttack/src/main/cpp/Fourbar.cpp
@@ -18,7 +18,7 @@ Fourbar* Fourbar::GetInstance() {
 }
 
 Fourbar::Fourbar() {
-    fourbarExtender = new rev::CANSparkMax(50, rev::CANSparkMaxLowLevel::MotorType::kBrushless);
+    fourbarExtender = new rev::CANSparkMax(50, rev::CANSparkMax::MotorType::kBrushless);
     fourbarSpeed = frc::SmartDashboard::GetNumber("fourbarSpeed", 0.1);
 
     fourbarEncoder = new rev::CANEncoder(*fourbarExtender);
@@ -26,46 +26,68 @@ Fourbar::Fourbar() {
     extended = new frc::DigitalInput(0);
     retracted = new frc::DigitalInput(1);
 
-    retractedTripped = true;
-    extendedTripped = false;
+    isPastRetracted = true;
+    isPastExtended = false;
+    isHoming = false;
+
+    rotationsSinceRetractTripped = 0.0;
 }
 
 void Fourbar::ExtendOrRetract(bool extendBut, bool retractBut){
 
     if (extendBut && !retractBut) {
-        if(extended->Get()){
+        isHoming = false;
+        if(!IsExtendedTripped() && !isPastExtended){
             fourbarExtender->Set(fourbarSpeed);
-            retractedTripped = false;
+            isPastRetracted = false;
         } 
-        // else if (!extended->Get()){
-        //     fourbarExtender->Set(0.0);
-        //     extendedTripped = true;
-        //     retractedTripped = false;
-        // }
+        else if (IsExtendedTripped() && !isPastExtended){
+            fourbarExtender->Set(0.0);
+            isPastExtended = true;
+        }
         else {
             fourbarExtender->Set(0.0);
         }
     }
     else if(retractBut && !extendBut){
-        if(retracted->Get()){
-            fourbarExtender->Set(fourbarSpeed); 
-            extendedTripped = false;
+        isHoming = false;
+        if(!IsRetractedTripped() && !isPastRetracted){
+            fourbarExtender->Set(-fourbarSpeed); 
+            isPastExtended = false;
         }
-        // else if (!retracted->Get()){
-        //     // fourbarExtender->Set(-fourbarSpeed*0.01);
-        //     retractedTripped = true;
-        // }
-        // else if(retracted->Get() && retractedTripped){
-        //     fourbarExtender->Set(0.0);
-        // }
+        else if (IsRetractedTripped() && !isPastRetracted){
+            fourbarExtender->Set(0.0);
+            isPastRetracted = true;
+        }
         else{
             fourbarExtender->Set(0.0);
         }
     }
+    else if (isHoming){}
+    
     else {
         fourbarExtender->Set(0.0);
     }
     
+}
+
+void Fourbar::FourbarHome(bool homingBut){
+    if ((homingBut || isHoming) && !IsRetractedTripped() && !isPastRetracted){
+        isHoming = true;
+        fourbarExtender->Set(-0.1);
+    }
+    else if (IsRetractedTripped() && !isPastRetracted && isHoming) {
+        isPastRetracted = true;
+        rotationsAtRetractTripped = fourbarEncoder->GetPosition();
+        fourbarExtender->Set(-0.02);
+    }
+    else if (-rotationsSinceRetractTripped < ROTATIONS_TO_HOME && isHoming){
+        rotationsSinceRetractTripped = fourbarEncoder->GetPosition() - rotationsAtRetractTripped;
+        fourbarExtender->Set(-0.02);
+    }
+    else {
+        isHoming = false;
+    }
 }
 
 void Fourbar::UpdateFourbarSpeed() {
@@ -81,6 +103,17 @@ void Fourbar::UpdateFourbarSpeed() {
 
 void Fourbar::PrintClimberRPM(){
 
+    frc::SmartDashboard::PutNumber("Fourbar Encoder Position", fourbarEncoder->GetPosition());
+    frc::SmartDashboard::PutNumber("Fourbar Rotations To Home", ROTATIONS_TO_HOME);
     frc::SmartDashboard::PutNumber("Fourbar Current", fourbarExtender->GetOutputCurrent());
     frc::SmartDashboard::PutNumber("Fourbar RPM", fourbarEncoder->GetVelocity());
+}
+
+//Private Methods
+bool Fourbar::IsExtendedTripped(){
+    return !retracted->Get();
+}
+
+bool Fourbar::IsRetractedTripped(){
+    return !extended->Get();
 }
--- SharkAttack/src/main/include/Fourbar.h
+++ SharkAttack/src/main/include/Fourbar.h
@@ -5,13 +5,14 @@
 #include <frc/Ultrasonic.h>
 #include <frc/DigitalInput.h>
 #include <rev/CANEncoder.h>
+#include <rev/CANPIDController.h>
 
 class Fourbar{
     public:
         static Fourbar* GetInstance();  
         void ExtendOrRetract(bool extendBut, bool retractBut);
         void UpdateFourbarSpeed();
-
+        void FourbarHome(bool homingBut);
         void PrintClimberRPM();
 
     private:
@@ -19,6 +20,9 @@ class Fourbar{
         static Fourbar* s_instance;
 
         Fourbar(); 
+
+        bool IsExtendedTripped();
+        bool IsRetractedTripped();
     
         rev::CANSparkMax *fourbarExtender;
         rev::CANEncoder *fourbarEncoder;
@@ -29,8 +33,17 @@ class Fourbar{
         frc::DigitalInput *extended;
         frc::DigitalInput *retracted;
 
-        bool retractedTripped;
-        bool extendedTripped;
+        bool isPastRetracted;
+        bool isPastExtended;
+        bool isHoming;
+
+        double rotationsSinceRetractTripped;
+        double rotationsAtRetractTripped;
+
+        const double DESIRED_MM_TO_HOME = 1.0;
+        const double GEAR_RATIO = 4.0;
+        const double ROTATIONS_TO_HOME = DESIRED_MM_TO_HOME * GEAR_RATIO;
+
                     
         
 };
