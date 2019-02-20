9b8b71f3737f5458c5eb422aa5b42d9b24a94cef
sebmolina <colombiasm24@gmail.com>
2019-02-19 19:46:45 -0500

    fixed build errors

--- SharkAttack/src/main/cpp/Robot.cpp
+++ SharkAttack/src/main/cpp/Robot.cpp
@@ -132,37 +132,37 @@ void Robot::TeleopPeriodic() {
     arm->CheckHatchGripper(oi->GetIsGripperClosed());
   }
 
-  if(oi->SetPresetToAButton()){
-    arm->MoveArmToPosition(oi->ArmPresetLow(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
-  }
+  // if(oi->SetPresetToAButton()){
+  //   arm->MoveArmToPosition(oi->ArmPresetLow(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
+  // }
 
-  if(oi->SetPresetToBButton()){
-    arm->MoveArmToPosition(oi->ArmPresetMid(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
-  }
+  // if(oi->SetPresetToBButton()){
+  //   arm->MoveArmToPosition(oi->ArmPresetMid(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
+  // }
 
-  if(oi->SetPresetToXButton()){
-    arm->MoveArmToPosition(oi->ArmPresetHigh(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
-  }
+  // if(oi->SetPresetToXButton()){
+  //   arm->MoveArmToPosition(oi->ArmPresetHigh(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
+  // }
 
-  if(oi->SetPresetToYButton()){
-    arm->MoveArmToPosition(oi->ArmPresetPickupCargo(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
-  }
+  // if(oi->SetPresetToYButton()){
+  //   arm->MoveArmToPosition(oi->ArmPresetPickupCargo(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
+  // }
 
-  if(oi->SetPresetToDPadUp()){
-    arm->MoveArmToPosition(oi->ArmPresetNegLow(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
-  }
+  // if(oi->SetPresetToDPadUp()){
+  //   arm->MoveArmToPosition(oi->ArmPresetNegLow(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
+  // }
 
-  if(oi->SetPresetToDPadDown()){
-    arm->MoveArmToPosition(oi->ArmPresetNegMid(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
-  }
+  // if(oi->SetPresetToDPadDown()){
+  //   arm->MoveArmToPosition(oi->ArmPresetNegMid(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
+  // }
 
-  if(oi->SetPresetToDPadLeft()){
-    arm->MoveArmToPosition(oi->ArmPresetNegHigh(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
-  }
+  // if(oi->SetPresetToDPadLeft()){
+  //   arm->MoveArmToPosition(oi->ArmPresetNegHigh(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
+  // }
 
-  if(oi->SetPresetToDPadRight()){
-    arm->MoveArmToPosition(oi->ArmPresetNegPickupCargo(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
-  }
+  // if(oi->SetPresetToDPadRight()){
+  //   arm->MoveArmToPosition(oi->ArmPresetNegPickupCargo(), drivetrain->GetWristEncoderValue(), drivetrain->GetArmEncoderValue());
+  // }
 }
 
 void Robot::DisabledInit() {
