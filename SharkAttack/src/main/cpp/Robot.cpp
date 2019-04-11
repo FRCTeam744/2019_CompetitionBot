c16355ad0079c41cb182e4fdadd6c8adc92232f9
arifire21 <ariwolfpup@icloud.com>
2019-04-10 21:16:17 -0400

    started mid hatch rocket auto, commented out

--- SharkAttack/src/main/cpp/Robot.cpp
+++ SharkAttack/src/main/cpp/Robot.cpp
@@ -181,6 +181,10 @@ void Robot::AutonomousInit()
     autoPathNames.push_back("LeftPlatformToLeftFrontRocket");
     autoPathDirections.push_back(drivetrain->FORWARD);
     autoArmPresets.push_back(oi->FRONT_HIGH_HATCH_POSITION);
+
+    // autoPathNames.push_back("LeftPlatformToLeftFrontRocketMid");
+    // autoPathDirections.push_back(drivetrain->FORWARD);
+    // autoArmPresets.push_back(oi->FRONT_MID_HATCH_POSITION);
   
     autoPathNames.push_back("LeftFrontRocketToLeftLoadingStation");
     autoPathDirections.push_back(drivetrain->REVERSE);
